8f08cfc4f7bf6117a83cda561a0f7de348c3fc40
MasaKusuda <masa.kusuda.1999@gmail.com>
2023-05-10 19:11:21 +0900

    add:api_test code

---
+++ src/api_test.py
@@ -0,0 +1,31 @@
+#%%
+import os
+import openai
+openai.api_key_path = "../env"
+print(openai.Model.list())
+#%%
+from pdfminer.high_level import extract_text
+pdf_text = extract_text('../sample.pdf').replace('\n','')
+
+
+
+#%%
+return_obj = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role": "user", "content": "compress the below content. " + pdf_text},
+    ]
+)
+print(return_obj["choices"][0]["message"]["content"])
+print(return_obj["usage"])
+# %%
+compressd_context = return_obj["choices"][0]["message"]["content"]
+return_obj = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo",
+  messages=[
+        {"role":"assistant", "content":"context of the paper is below." + compressd_context},
+        {"role": "user", "content": "tell me what is the new point of this paper" }
+    ]
+)
+print(return_obj["choices"][0]["message"]["content"])
+print(return_obj["usage"])
